2935a0af589886bc9b30615f2430ef1cb44b0cf1
DJTommek <tomas@palider.cz>
2020-06-21 20:42:15 +0200

    Updated index text for TG module

--- index.php
+++ index.php
@@ -11,9 +11,10 @@ require_once __DIR__ . '/src/config.php';
 	<form method="GET" action="modules/telegramSendMessage.php" target="_blank">
 		<label>
 			If you want to send test message, setup 'TELEGRAM_BOT_TOKEN' in data/config.local.php and put some Telegram chat ID here:<br>
-			<input type="text" name="telegramChatId" value="1489532856">
+			<input type="text" name="telegramChatId" value="148953285">
 			<button type="submit">Odeslat</button>
 		</label>
+		<br>Btw, predefined value is <a href="https://t.me/DJTommek" target="_blank" title="DJTommek's Teleegram">my TG</a> so you can ask me for help :)
 		<p>
 			Note: User has to allow your bot to talk in that chat.<br>
 			For example if chat ID is your private chat, you need to start conversation with that bot.<br>
