f8d85ef21a3247e3978dc2f13ff31d2381d61643
DJTommek <tomas@palider.cz>
2020-07-03 17:08:48 +0200

    Added Telegram set webhook module

--- index.php
+++ index.php
@@ -8,6 +8,7 @@ require_once __DIR__ . '/src/config.php';
 		Hello world! <?= Icons::CHECKED; ?>
 	</p>
 	<h2>Module Telegram</h2>
+	<h3>Send message</h3>
 	<form method="GET" action="modules/telegramSendMessage.php" target="_blank">
 		<label>
 			If you want to send test message, setup 'TELEGRAM_BOT_TOKEN' in data/config.local.php and put some Telegram chat ID here:<br>
@@ -21,4 +22,18 @@ require_once __DIR__ . '/src/config.php';
 			To send message to group chat or channel, bot has to be in this chat and have at least send message permission.
 		</p>
 	</form>
+	<h3>Setup webhook</h3>
+	<p>
+		<a href="modules/telegramSetWebhook.php" target="_blank">This script</a>
+		will automatically setup webhook to
+		<a href="modules/telegramWebhook.php" target="_blank">this URL</a>
+		but only if it is HTTPS, otherwise you will be rejected from Telegram servers.<br>
+		See <a href="https://github.com/unreal4u/telegram-api/wiki/Getting-updates-via-Webhook" target="_blank">unreal4u/telegram-api</a> for more info.
+	</p>
+	<p>
+		If you successfully setup webhook, you can send message to your bot from Telegram and you should get reply.
+	</p>
+	<p>
+		Note: If you are not able to use webhook, there is possible to setup polling.
+	</p>
 <?php
