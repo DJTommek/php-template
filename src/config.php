dc8f6017aadbc82a7d0e71fd64189509797b983a
DJTommek <tomas@palider.cz>
2020-09-04 20:52:37 +0200

    Changes from https://github.com/DJTommek/better-location (DummyLogger, errors to email and few minor changes)

--- src/config.php
+++ src/config.php
@@ -1,40 +1,39 @@
 <?php
-/**
- * Convert all errors to exceptions
- */
-
-use Tracy\Debugger;
-
-error_reporting(E_ALL);
-
-DEFINE('LOG_FOLDER', __DIR__ . '/../data/log/');
+DEFINE('FOLDER_DATA', __DIR__ . '/../data');
 DEFINE('DATE_FORMAT', 'Y-m-d');
 DEFINE('TIME_FORMAT', 'H:i:s');
 DEFINE('DATETIME_FORMAT', DATE_FORMAT . ' ' . TIME_FORMAT);
+DEFINE('DATETIME_FORMAT_ZONE', DATE_FORMAT . ' ' . TIME_FORMAT . ' T');
+DEFINE('LOG_ID', rand(10000, 99999));
 
 // @TODO in case of error, show some info about renaming config.local.example.php to config.local.php
-require_once __DIR__ . '/../data/config.local.php';
+require_once FOLDER_DATA . '/config.local.php';
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-Tracy\Debugger::enable(DEVELOPMENT_IPS, __DIR__ . '/../data/log/');
+Tracy\Debugger::enable(DEVELOPMENT_IPS, FOLDER_DATA . '/tracy-log/');
 Tracy\Debugger::$strictMode = true;
 Tracy\Debugger::$logSeverity = E_NOTICE | E_WARNING;
 
+if (defined('TRACY_DEBUGGER_EMAIL') && is_null(TRACY_DEBUGGER_EMAIL) === false) {
+	Tracy\Debugger::$email = TRACY_DEBUGGER_EMAIL;
+}
+
 /**
- * @param $className
+ * @param string $className
  * @throws Exception
  */
-function my_autoloader($className) {
+function dummyAutoloader(string $className): void {
 	$path = str_replace('\\', '/', $className);
-	$file = __DIR__ . '/libs/' . $path . '.php';
+	$file = str_replace('\\', '/', __DIR__) . '/libs/' . $path . '.php';
 	if (file_exists($file)) {
-		require $file;
+		require_once $file;
 	} else {
-		throw new Exception('file does not exists');
+		throw new \Exception(sprintf('Class "%s" cannot be loaded, file "%s" does not exists.', $path, $file));
 	}
 }
 
-spl_autoload_register('my_autoloader');
+spl_autoload_register('dummyAutoloader');
 
-Debugger::log('Request: ' . ($_SERVER['REMOTE_ADDR'] ? $_SERVER['REMOTE_ADDR'] . ' - ' : '') . $_SERVER['REQUEST_URI'], Debugger::DEBUG);
+// @TODO this is a lot of data to log but it's ok for alpha/beta phase. Probably should be removed in stable.
+DummyLogger::log(DummyLogger::NAME_ALL_REQUESTS, $_SERVER);
---
+++ src/libs/DummyLogger.php
@@ -0,0 +1,44 @@
+<?php
+
+/**
+ * Log manager.
+ */
+class DummyLogger
+{
+
+	const NAME_ALL_REQUESTS = 'request';
+
+	/**
+	 * One JSON per line
+	 *
+	 * @see http://jsonlines.org/
+	 * @see https://github.com/wardi/jsonlines
+	 */
+	const FILE_EXTENSION = 'jsonl'; //
+	const LINE_SEPARATOR = PHP_EOL;
+
+	public static function log(string $name, $content): void {
+		if (!preg_match('/^[a-zA-Z0-9_]{1,30}$/', $name)) {
+			throw new \InvalidArgumentException('Invalid log name.');
+		}
+		$name = mb_strtolower($name);
+		$path = sprintf('%s/log/%s', FOLDER_DATA, $name);
+		if (!file_exists($path)) {
+			mkdir($path, 0750, true);
+		}
+
+		$writeLogObject = new \stdClass();
+		$now = new \DateTimeImmutable();
+		$writeLogObject->datetime = $now->format(DATE_ISO8601);
+		if (defined('LOG_ID')) {
+			$writeLogObject->log_id = LOG_ID;
+		}
+		$writeLogObject->name = $name;
+		$writeLogObject->content = $content;
+		file_put_contents(
+			sprintf('%s/%s_%s.%s', $path, $name, $now->format(DATE_FORMAT), self::FILE_EXTENSION),
+			json_encode($writeLogObject) . self::LINE_SEPARATOR,
+			FILE_APPEND
+		);
+	}
+}
