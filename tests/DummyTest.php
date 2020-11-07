c19ef6d11c9c585817e882c5b8e2e0e11357b059
Tomas Palider (DJTommek) <tomas@palider.cz>
2020-11-07 15:52:14 +0100

    Added phpunit and example dummy test

---
+++ tests/DummyTest.php
@@ -0,0 +1,29 @@
+<?php declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Example dummy test.
+ */
+final class DummyTest extends TestCase
+{
+	private $dummyVariable;
+
+	public function setUp(): void
+	{
+		$this->dummyVariable = 'setUp';
+	}
+
+	public function testDummyVariable(): void
+	{
+		$this->assertEquals('Coordinate is 49.123000', sprintf('Coordinate is %F', 49.123));
+		$this->assertEquals('setUp', $this->dummyVariable);
+	}
+
+	public function testWithException(): void
+	{
+		$this->expectException(\InvalidArgumentException::class);
+		$this->expectExceptionMessage('Blah blah...');
+		throw new \InvalidArgumentException('Blah blah...');
+	}
+}
