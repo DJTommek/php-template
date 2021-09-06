073eb2a06d980b29c4055a6013fddfc1a9b464cb
Tomas Palider (DJTommek) <tomas@palider.cz>
2021-09-06 21:51:59 +0200

    Database: refactored, added fix to prepared statements, removed queryArray, added helper constants

--- src/libs/Database.php
+++ src/libs/Database.php
@@ -2,19 +2,41 @@
 
 namespace App;
 
+use Tracy\Debugger;
+
 class Database
 {
-	/**
-	 * @var \PDO to database
-	 */
+	/** @var string Randomly occuring error on WEDOS webhosting */
+	private const PDO_REPREPARED_ERROR = 'SQLSTATE[HY000]: General error: 1615 Prepared statement needs to be re-prepared';
+
+	public const TRUE = 1;
+	public const FALSE = 0;
+
+	public const DISABLED = 0;
+	public const ENABLED = 1;
+	public const DELETED = 2;
+
+	public const ORDER_ASC = 'ASC';
+	public const ORDER_DESC = 'DESC';
+	public const ORDERS = [
+		self::ORDER_ASC,
+		self::ORDER_DESC,
+	];
+
+	/** @var \PDO */
 	private $db;
 
-	public function __construct(string $db_server, string $db_schema, string $db_user, string $db_pass, string $db_charset = 'utf8mb4')
+	public function __construct(string $server, string $schema, string $user, string $pass, $charset = 'utf8mb4')
 	{
-		$dsn = 'mysql:host=' . $db_server . ';dbname=' . $db_schema . ';charset=' . $db_charset;
-		$this->db = new \PDO($dsn, $db_user, $db_pass);
+		$dsn = sprintf('mysql:host=%s;dbname=%s;charset=%s', $server, $schema, $charset);
+		$this->db = new \PDO($dsn, $user, $pass);
+		// Fetch each row as array indexed by column name
+		$this->db->setAttribute(\PDO::ATTR_DEFAULT_FETCH_MODE, \PDO::FETCH_ASSOC);
+		// Return int and float columns as PHP int and float types
 		$this->db->setAttribute(\PDO::ATTR_STRINGIFY_FETCHES, false);
+		// https://stackoverflow.com/questions/10113562/pdo-mysql-use-pdoattr-emulate-prepares-or-not
 		$this->db->setAttribute(\PDO::ATTR_EMULATE_PREPARES, false);
+		// Throw \PDOException in case of error. See https://www.php.net/manual/en/class.pdoexception.php
 		$this->db->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
 		// Fix if database server don't have enabled STRICT_ALL_TABLES. See https://stackoverflow.com/questions/27880035/what-causes-mysql-not-to-enforce-not-null-constraint
 		$this->db->query('SET SESSION SQL_MODE=STRICT_ALL_TABLES');
@@ -28,24 +50,26 @@ class Database
 	/**
 	 * Shortcut for prepared statement
 	 *
-	 * @param string $query
-	 * @param mixed ...$params
+	 * @param string $query SQL query
+	 * @param mixed ...$params Optional parameters for fill prepared statements
 	 * @return bool|\PDOStatement
 	 */
 	public function query(string $query, ...$params)
 	{
-		$sql = $this->db->prepare($query);
-		$sql->execute($params);
-		return $sql;
+		try {
+			return $this->queryReal($query, $params);
+		} catch (\PDOException $exception) {
+			if ($exception->getMessage() === self::PDO_REPREPARED_ERROR) {
+				Debugger::log('Catched PDO re-prepared error, trying again...', Debugger::WARNING);
+				Debugger::log($exception, Debugger::WARNING);
+				return $this->queryReal($query, $params);
+			} else {
+				throw $exception;
+			}
+		}
 	}
 
-	/**
-	 * Array shortcut for prepared statement
-	 *
-	 * @param mixed[] $params
-	 * @return bool|\PDOStatement
-	 */
-	public function queryArray(string $query, array $params)
+	private function queryReal(string $query, $params)
 	{
 		$sql = $this->db->prepare($query);
 		$sql->execute($params);
